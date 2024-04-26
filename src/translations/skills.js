9eb577046863091b33c6e6171ac90889b99a5b45
Felipe Rodrigues <neath.felipe@gmail.com>
2024-04-25 22:56:00 -0300

    feat: create container skills

---
+++ src/translations/skills.js
@@ -0,0 +1,12 @@
+const translations = {
+  pt: {
+    title: 'Habilidades',
+    subTitle: 'Superpoderes?',
+  },
+  en: {
+    title: 'Skills',
+    subTitle: 'Superpowers?',
+  },
+};
+
+export default translations;
